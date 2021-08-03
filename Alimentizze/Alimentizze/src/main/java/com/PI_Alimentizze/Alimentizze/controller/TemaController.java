cd3e2da907c68848c85c70b33001857d0a81ad69
Gui Matheus <guilherme.maths.sil@gmail.com>
2021-08-03 12:05:06 -0300

    Criaçao de Controller e atualizaçao do Repositorio

---
+++ Alimentizze/Alimentizze/src/main/java/com/PI_Alimentizze/Alimentizze/controller/TemaController.java
@@ -0,0 +1,39 @@
+package com.PI_Alimentizze.Alimentizze.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.PI_Alimentizze.Alimentizze.modelo.Tema;
+import com.PI_Alimentizze.Alimentizze.repositorio.TemaRepositorio;
+
+
+
+@RestController
+@RequestMapping("/tema")
+@CrossOrigin("*")
+public class TemaController
+{
+
+	@Autowired
+	private TemaRepositorio repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Tema>> GetAll() 
+	{
+		return ResponseEntity.status(200).body(repository.findAll());
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<Tema>GetById(@PathVariable long id)
+	{
+		return repository.findById(id).map(resp -> ResponseEntity.status(200).body(resp))
+				.orElse(ResponseEntity.status(404).build());
+	}
+	
+}
