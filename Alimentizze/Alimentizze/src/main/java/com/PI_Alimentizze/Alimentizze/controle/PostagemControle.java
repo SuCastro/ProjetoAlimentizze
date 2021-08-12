16e4db3609d5c68bced1f3c889ed1d8f0c908d81
Felipe Pereira de Souza <felipepereirasouza@outlook.com>
2021-08-12 10:44:26 -0300

    Alteração metodos controller para autenticação

--- Alimentizze/Alimentizze/src/main/java/com/PI_Alimentizze/Alimentizze/controle/PostagemControle.java
+++ Alimentizze/Alimentizze/src/main/java/com/PI_Alimentizze/Alimentizze/controle/PostagemControle.java
@@ -1,9 +1,12 @@
 package com.PI_Alimentizze.Alimentizze.controle;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+//import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.PI_Alimentizze.Alimentizze.modelo.Postagem;
 import com.PI_Alimentizze.Alimentizze.repositorio.PostagemRepositorio;
+import com.PI_Alimentizze.Alimentizze.servico.PostagemServico;
 
 	@RestController
 	@RequestMapping("/Postagem")
@@ -26,6 +30,10 @@ import com.PI_Alimentizze.Alimentizze.repositorio.PostagemRepositorio;
 		@Autowired
 		private PostagemRepositorio repository;
 
+		@Autowired
+		private PostagemServico servicos;
+
+			
 		@GetMapping
 		public ResponseEntity<List<Postagem>> GetAll() {
 			return ResponseEntity.status(200).body(repository.findAll());
@@ -38,24 +46,67 @@ import com.PI_Alimentizze.Alimentizze.repositorio.PostagemRepositorio;
 		}
 		
 		@GetMapping("/titulo/{titulo}")
-		public ResponseEntity<List<Postagem>> GetBy(@PathVariable String titulo) {
+		public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) {
 			return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 		}
 		
-		@PostMapping
+		
+		/*  Antigo metodo Post /@PostMapping
 		public ResponseEntity<Postagem> post(@RequestBody Postagem titulo) {
 			return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(titulo));
+		}*/
+		
+		@PostMapping("/salvar")
+		public ResponseEntity<Object> cadastrarPostagem(@Valid @RequestBody Postagem novaPostagem) {
+			Optional<?> objetoCadastrado = servicos.cadastrarPostagem(novaPostagem);
+
+			if (objetoCadastrado.isPresent()) {
+				return ResponseEntity.status(201).body(objetoCadastrado.get());
+			} else {
+				return ResponseEntity.status(400).build();
+			}
+
 		}
 
-		@PutMapping 
+		
+
+		/*  Antigo metodo PutMapping/@PutMapping 
 		public ResponseEntity<Postagem> put(@RequestBody Postagem titulo){
 			return ResponseEntity.status(HttpStatus.OK).body(repository.save(titulo));
 			
+		}*/
+		
+		@PutMapping("/alterar")
+		public ResponseEntity<Object> alterar(@Valid @RequestBody Postagem postagemParaAlterar) {
+			Optional<Postagem> objetoAlterado = servicos.alterarPostagem(postagemParaAlterar);
+
+			if (objetoAlterado.isPresent()) {
+				return ResponseEntity.status(201).body(objetoAlterado.get());
+			} else {
+				return ResponseEntity.status(400).build();
+			}
 		}
 
-		@DeleteMapping("/{id}")
+
+		/* Delete void /@DeleteMapping("/{id}")
 		public void delete(@PathVariable Long id) {
 			repository.deleteById(id);
+		}*/
+		
+		@DeleteMapping("/deletar/{id}")
+		public ResponseEntity<Object> deletarPorId(@PathVariable(value = "id") Long id) {
+			Optional<Postagem> objetoExistente = repository.findById(id);
+			if (objetoExistente.isPresent()) {
+				repository.deleteById(id);
+				return ResponseEntity.status(200).build();
+			} else {
+				return ResponseEntity.status(400).build();
+			}
+
 		}
+
+		
+		
+		
 		
 }
--- Alimentizze/Alimentizze/src/main/java/com/PI_Alimentizze/Alimentizze/modelo/Usuario.java
+++ Alimentizze/Alimentizze/src/main/java/com/PI_Alimentizze/Alimentizze/modelo/Usuario.java
@@ -1,13 +1,20 @@
 package com.PI_Alimentizze.Alimentizze.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table( name = "Usuario")
@@ -33,6 +40,25 @@ public class Usuario
 	
 	@NotBlank (message = "obrigatório um tipo de usuario")
 	private String tipoDeUsuario;
+
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties({"usuario"})
+	private List<Postagem> minhasPostagens = new ArrayList<>();
+
+	public Usuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Usuario(Long id, String nomeCompleto, String email,String senha, String tipoDeUsuario) {
+		super();
+		this.id = id;
+		this.nomeCompleto = nomeCompleto;
+		this.email = email;
+		this.senha = senha;
+		this.tipoDeUsuario = tipoDeUsuario;
+	}
 
 
 	public Long getId()
@@ -93,6 +119,16 @@ public class Usuario
 	{
 		this.tipoDeUsuario = tipoDeUsuario;
 	}
-	
+
+
+	public List<Postagem> getMinhasPostagens() {
+		return minhasPostagens;
+	}
+
+
+	public void setMinhasPostagens(List<Postagem> minhasPostagens) {
+		this.minhasPostagens = minhasPostagens;
+	}
+	 
 	
 }
