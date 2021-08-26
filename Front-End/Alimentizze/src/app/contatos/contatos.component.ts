98ff1195f568ad73525dcb46ed2cfcdaf8d7d20f
Gui Matheus <guilherme.maths.sil@gmail.com>
2021-08-26 13:13:33 -0300

    Criacao do projeto angular com componentes menu, contato, rodape e sobre-nos

---
+++ Front-End/Alimentizze/src/app/contatos/contatos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contatos',
+  templateUrl: './contatos.component.html',
+  styleUrls: ['./contatos.component.css']
+})
+export class ContatosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
