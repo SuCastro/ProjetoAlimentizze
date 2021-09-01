0f3898ca50a4c9e42648b21b08ab1cf863168bad
Stelvio Alexandre Smith <silviosmith18@gmail.com>
2021-09-01 11:25:13 -0300

    implentando a Classe model

---
+++ Front-End/Alimentizze/src/app/modelo/Usuario.ts
@@ -0,0 +1,14 @@
+import { Postagem } from "./Postagem"
+
+export class Usuario{
+
+    public id: number
+    public email: string
+    public foto: string
+    public nomeCompleto: string
+    public tipoDeUsuario: string 
+    public senha:string
+    public minhasPostagens: Postagem[]
+
+
+}
---
+++ Front-End/Alimentizze/src/app/modelo/UsuarioDTO.ts
@@ -0,0 +1,11 @@
+export class UsuarioDTO{
+
+    
+    public id: number
+    public email: string
+    public foto: string
+    public nomeCompleto: string
+    public tipoDeUsuario: string 
+    public senha:string 
+    public token:string
+}
