e663ab2b557caa2e0f3dd711ef58353f398801be
TamiresGS <tamires_app@hotmail.com>
2021-09-08 18:30:54 -0300

    Implementando Service de Tema

--- Front-End/Alimentizze/src/app/modelo/Tema.ts
+++ Front-End/Alimentizze/src/app/modelo/Tema.ts
@@ -2,8 +2,8 @@ import { Postagem } from "./Postagem"
 
 export class Tema{
 
-
-    public categoria:string
+    // public categoria:string
+    public descricao:string
     public id:number
     public postagem: Postagem []
 }
